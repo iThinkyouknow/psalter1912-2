7deebd14bf7b35aca97e44c82f5ce8e629e4fb6c
iThinkyouknow <woontlt.dev@gmail.com>
2018-02-19 11:30:49 +0800

    refactored transformer

--- data/data-transformers/confessions-forms-transformer.js
+++ data/data-transformers/confessions-forms-transformer.js
@@ -8,6 +8,18 @@ const j = (text) => {
 
 const common = require('./common');
 
+const {
+        line_is_valid,
+        compose,
+        proof_super_script_fn,
+        ans_fn,
+        q_fn,
+        italics_fn,
+        bold_fn,
+        list_fn,
+        text_trimmer
+      } = common;
+
 /**
  * {
  *      title,
@@ -83,16 +95,6 @@ const common = require('./common');
 
 
 const text_attributor = style => text => {
-    const {
-        compose,
-        proof_super_script_fn,
-        ans_fn,
-        q_fn,
-        italics_fn,
-        bold_fn,
-        list_fn,
-        text_trimmer
-    } = common;
 
     const style_obj = (style !== undefined) ? {
         [style]: true,
@@ -117,38 +119,21 @@ const text_attributor = style => text => {
 
 const regex = /(\n\n)|(\n)|(#\d+#)|(^ *A\. +)|(Q\. \d+\.+)|(<([\w]+)[^>]*>.*?<\/\7>)|(?:^|\n\n| *)(\d+\.) +/g;
 
-[
-    // [require('../_The-Heidelberg-Catechism.json'), 'The-Heidelberg-Catechism(by-LD)']
-    // [require('../_The-Belgic-Confession.json'), 'The-Belgic-Confession']
-    // [require('../_The-Apostles-Creed.json'), 'The-Apostles-Creed']
-    // [require('../_The-Athanasian-Creed.json'), 'The-Athanasian-Creed'],
-    // [require('../_The-Creed-of-Chalcedon.json'), 'The-Creed-of-Chalcedon'],
-    [require('../_Form-for-the-Administration-of-Baptism.json'), 'Form-for-the-Administration-of-Baptism'],
-    // [require('../_The-Nicene-Creed.json'), 'The-Nicene-Creed'],
-    // [require('../_Form-for-Excommunication.json'), 'Form-for-Excommunication'],
-    // [require('../_Form-for-Public-Confession-of-Faith.json'), 'Form-for-Public-Confession-of-Faith'],
-    // [require('../_Form-for-Readmitting-Excommunicated-Persons.json'), 'Form-for-Readmitting-Excommunicated-Persons'],
-    // [require('../_Form-for-the-Administration-of-the-Lord\'s-Supper.json'), 'Form-for-the-Administration-of-the-Lord\'s-Supper'],
-    // [require('../_Form-for-the-Confirmation-of-Marriage-before-the-Church.json'), 'Form-for-the-Confirmation-of-Marriage-before-the-Church'],
-    // [require('../_Form-for-the-Installation-of-Professors-of-Theology.json'), 'Form-for-the-Installation-of-Professors-of-Theology'],
-    // [require('../_Form-for-the-Ordination-(or-Installation)-of-Missionaries.json'), 'Form-for-the-Ordination-(or-Installation)-of-Missionaries'],
-    // [require('../_Form-of-Ordination-(or-Installation)-of-Ministers-of-God\'s-Word.json'), 'Form-of-Ordination-(or-Installation)-of-Ministers-of-God\'s-Word'],
-    // [require('../_Form-of-Ordination-of-Elders-and-Deacons.json'), 'Form-of-Ordination-of-Elders-and-Deacons'],
-    // [require('../_Formula-of-Subscription-(PRCA).json'), 'Formula-of-Subscription-(PRCA)'],
-].forEach(([file, output_name]) => {
+
+const reformatter_fn = regex => ([file, output_name]) => {
     const content_array = Object.entries(file.content);
 
     const new_content = content_array.map(([key, {header, body, chapter, proof}]) => {
 
         const new_header = header
             .split(regex)
-            .filter(common.line_is_valid)
+            .filter(line_is_valid)
             .map(text_attributor('is_bold'));
         //end of new_header
 
         const new_body = body
             .split(regex)
-            .filter(common.line_is_valid)
+            .filter(line_is_valid)
             .map(text_attributor());
         //end of new_body
 
@@ -157,7 +142,7 @@ const regex = /(\n\n)|(\n)|(#\d+#)|(^ *A\. +)|(Q\. \d+\.+)|(<([\w]+)[^>]*>.*?<\/
         const new_proof = (proof || '')
             .replace(proof_regex, (m, p1, p2) => `${p1}\n${p2}`)
             .split(regex)
-            .filter(common.line_is_valid)
+            .filter(line_is_valid)
             .map(text_attributor());
 
         const new_content = [new_header, new_body, new_proof];
@@ -169,7 +154,7 @@ const regex = /(\n\n)|(\n)|(#\d+#)|(^ *A\. +)|(Q\. \d+\.+)|(<([\w]+)[^>]*>.*?<\/
             header: (key.toLowerCase() === 'introduction') ? header : chapter,
             content: new_content,
             id: (typeof key_int === 'number') ? key_int : key
-    }
+        }
     });
 
 // log(j(new_content));
@@ -227,6 +212,26 @@ const regex = /(\n\n)|(\n)|(#\d+#)|(^ *A\. +)|(Q\. \d+\.+)|(<([\w]+)[^>]*>.*?<\/
 
     fs.writeFileSync(`/Users/notforyoutouse/psalter1912-2/data/${output_name}.json`, json_new_creed);
 
-});
+};
+
+[
+    // [require('../_The-Heidelberg-Catechism.json'), 'The-Heidelberg-Catechism(by-LD)']
+    // [require('../_The-Belgic-Confession.json'), 'The-Belgic-Confession']
+    // [require('../_The-Apostles-Creed.json'), 'The-Apostles-Creed']
+    // [require('../_The-Athanasian-Creed.json'), 'The-Athanasian-Creed'],
+    // [require('../_The-Creed-of-Chalcedon.json'), 'The-Creed-of-Chalcedon'],
+    [require('../_Form-for-the-Administration-of-Baptism.json'), 'Form-for-the-Administration-of-Baptism'],
+    // [require('../_The-Nicene-Creed.json'), 'The-Nicene-Creed'],
+    // [require('../_Form-for-Excommunication.json'), 'Form-for-Excommunication'],
+    // [require('../_Form-for-Public-Confession-of-Faith.json'), 'Form-for-Public-Confession-of-Faith'],
+    // [require('../_Form-for-Readmitting-Excommunicated-Persons.json'), 'Form-for-Readmitting-Excommunicated-Persons'],
+    // [require('../_Form-for-the-Administration-of-the-Lord\'s-Supper.json'), 'Form-for-the-Administration-of-the-Lord\'s-Supper'],
+    // [require('../_Form-for-the-Confirmation-of-Marriage-before-the-Church.json'), 'Form-for-the-Confirmation-of-Marriage-before-the-Church'],
+    // [require('../_Form-for-the-Installation-of-Professors-of-Theology.json'), 'Form-for-the-Installation-of-Professors-of-Theology'],
+    // [require('../_Form-for-the-Ordination-(or-Installation)-of-Missionaries.json'), 'Form-for-the-Ordination-(or-Installation)-of-Missionaries'],
+    // [require('../_Form-of-Ordination-(or-Installation)-of-Ministers-of-God\'s-Word.json'), 'Form-of-Ordination-(or-Installation)-of-Ministers-of-God\'s-Word'],
+    // [require('../_Form-of-Ordination-of-Elders-and-Deacons.json'), 'Form-of-Ordination-of-Elders-and-Deacons'],
+    // [require('../_Formula-of-Subscription-(PRCA).json'), 'Formula-of-Subscription-(PRCA)'],
+].forEach(reformatter_fn(regex));
 
 
