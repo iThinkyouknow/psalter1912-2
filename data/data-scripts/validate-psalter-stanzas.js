203d24d213c54f5eec28b10a05e4521f0cc388de
iThinkyouknow <woontlt.dev@gmail.com>
2018-06-21 09:16:16 +0800

    fixed psalter typos

--- data/data-scripts/validate-psalter-stanzas.js
+++ data/data-scripts/validate-psalter-stanzas.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
 
+const psalterArray = require('../PsalterJSON.json');
+
+const problemPsalters = psalterArray
+    .map(({content}, j) => {
+        const problemStanza = content
+            .map((stanzasArray) => stanzasArray.length)
+            .reduce((acc, linesCount, index, array) => {
+                const validCount = array[0];
+                if (linesCount !== validCount) {
+                    return {
+                        problem: true,
+                        stanza: index + 1
+                        , psalter: j + 1
+                    }
+                }
+                return acc;
+            }, {problem: false, index: NaN, psalter: NaN});
+
+
+
+        console.dir(problemStanza, {colors: true});
+
+        return problemStanza;
+    })
+    .filter((problemObj) => problemObj.problem === true);
+
+
+console.dir(problemPsalters);
